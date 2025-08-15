7966519c2339379afbee14103146a5a6e6175892
yaninyzwitty <ianmwa143@gmail.com>
2025-08-15 22:35:06 +0300

    Add configuration and service setup for User service
    - Introduced .gitignore to exclude .env files.
    - Added config.yaml for service configuration, including user service and database settings.
    - Updated go.mod to include gopkg.in/yaml.v3 for YAML parsing.
    - Created main.go files for Like, Post, Outbox Processor, and User services, implementing basic server setup and graceful shutdown.
    - Implemented configuration loading in the User service using the new config package.

---
+++ internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log/slog"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	UserService UserService `yaml:"user-service"`
+	Database    Cocroach    `yaml:"cocroach-service"`
+}
+
+type UserService struct {
+	Port int `yaml:"port"`
+}
+
+type Cocroach struct {
+	Username string `yaml:"username"`
+	Host     string `yaml:"host"`
+	Database string `yaml:"database"`
+	Port     int    `yaml:"port"`
+}
+
+//  func to load config
+
+func (c *Config) LoadConfig(path string) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		slog.Error("failed to load file for path", "path", path, "error", err)
+	}
+
+	err = yaml.Unmarshal(data, c)
+	if err != nil {
+		slog.Error("failed to unmarshal config file", "error", err, "path", path)
+		return err
+	}
+
+	return nil
+}
